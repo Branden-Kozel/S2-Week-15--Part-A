1c8a0a94f3d13fb2a35216772690a379953f5ff6
Branden Kozel <branden.kozel@sinclair.edu>
2024-04-27 21:11:10 -0400

    Allows the user to input a Char to test along with a offset integer picked by the user. Various other small tweaks.

--- S2 Week 15- Part A/source.cpp
+++ S2 Week 15- Part A/source.cpp
@@ -29,18 +29,37 @@ char character(char start, int offset)
 int main() 
 {
     char userChar;
+    int numOffset = 1;
 
+    cout << "Testing Time!\nPlease enter a letter and then an offset number. \nEXAMPLE: "
+         << "character('a', 1) returns: " << character('a', 1) << endl
 
-    try {
-        cin >> userChar; cout << "  " << character(userChar, 1); cout << endl;
-    }
-    catch (invalidCharacterException) {
-        cout << "invalidCharacterException." << endl;
-    }
-    catch (invalidRangeException) {
-        cout << "invalidRangeException." << endl;
+         << "\nNow Please enter the letter you wish to test: "; cin >> userChar;
+    cout << "\nNow enter the offset number (Can be a negative number): "; cin >> numOffset;
+
+    try 
+    {
+        if (numOffset < 0)
+        {
+            cout << "The letter before " << userChar << " is: " << character(userChar, numOffset); cout << endl;
+        }
+
+        else 
+        {
+            cout << "The letter after " << userChar << " is: " << character(userChar, numOffset); cout << endl;
+        }
+       
     }
 
+    catch (invalidCharacterException) 
+    {
+        cout << "\ninvalidCharacterException! " << userChar << " is not valid. We only accept letters, a b c etc." << endl;
+    }
+
+    catch (invalidRangeException) 
+    {
+        cout << "\ninvalidRangeException! " << userChar << " and " << numOffset << " do not mix.\nThis combination goes outside of the alphabet!";
+    }
 
     return 0;
 }
